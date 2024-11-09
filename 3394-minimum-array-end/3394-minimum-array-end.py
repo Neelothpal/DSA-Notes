ce232d30c730ea2ad8d9050040f044838847bc3a
Neelothpal <106467796+Neelothpal@users.noreply.github.com>
2024-11-09 17:17:16 +0530

    Time: 7 ms (19.15%), Space: 16.6 MB (69.7%) - LeetHub

---
+++ 3394-minimum-array-end/3394-minimum-array-end.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        shifts = []
+        cur = x
+        calc = n - 1
+        
+        for i in range(32):
+            if not ((1 << i) & x):
+                shifts.append(i)
+        
+        for i in range(32, 64):
+            shifts.append(i)
+        
+        i = 0
+        while calc > 0:
+            cur += (calc & 1) << shifts[i]
+            calc >>= 1
+            i += 1
+            
+        return cur
